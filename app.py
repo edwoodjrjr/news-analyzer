9e61f802a847682cb6598444f90f2803af0a3dd1
Mike Shimkus <mikeshimkus@Mikes-MacBook-Pro.local>
2019-11-12 16:31:22 -0800

    updates

--- app.py
+++ app.py
@@ -3,9 +3,88 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
+# get news api key from env var
+# add web form to take input for news source, subject
+# move to new project and push to github
+
+import json
+import pprint
+import requests
+import math
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+from google.cloud import language_v1
+from google.cloud.language_v1 import enums
+
+@app.route("/insult")
+def insult():
+    #return app.send_static_file("index.html")
+    url = 'https://evilinsult.com/generate_insult.php?lang=en&type=json'
+    r = requests.get(url)
+    j = r.json()
+    #pprint.pprint(j)
+    #return j["insult"]
+    return sample_analyze_sentiment(j["insult"])
+
+@app.route("/news")
+def news():
+    url = ('https://newsapi.org/v2/top-headlines?'
+       #'q=Trump&'
+       'country=us&'
+       'language=en&'
+       'apiKey=189489ba7f074c52b390066575539c68')
+    r = requests.get(url)
+    j = r.json()
+    new_str = ''
+
+    for k,v in j.items():
+        if k == "articles":
+            articles_json = v
+            for article in articles_json:
+                description = str(article['description'])
+                new_str = new_str + description + ' '
+    
+    remove_table = dict.fromkeys(map(ord, '+"@#$'), None)
+    new_str = new_str.translate(remove_table)
+    new_str_len = len(new_str)
+
+    print('News: ' + new_str)
+    print('Number of Google Natural Language units used: ' + str(math.ceil(new_str_len/800)))
+    return new_str
+    #return sample_analyze_sentiment(new_str)
+
+def sample_analyze_sentiment(text_content):
+    """
+    Analyzing Sentiment in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    type_ = enums.Document.Type.PLAIN_TEXT
+
+    language = "en"
+    document = {"content": text_content, "type": type_, "language": language}
+
+    encoding_type = enums.EncodingType.UTF8
+
+    response = client.analyze_sentiment(document, encoding_type=encoding_type)
+
+    print(u"Response text: {}".format(text_content))
+    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    print(u"Document sentiment magnitude: {}".format(response.document_sentiment.magnitude))
+
+    # for sentence in response.sentences:
+    #     print(u"Sentence text: {}".format(sentence.text.content))
+    #     print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
+    #     print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
+
+    print(u"Language of the text: {}".format(response.language))
+
+    response_json = {"text": text_content, "sentiment_score": response.document_sentiment.score, "sentiment_magnitude": response.document_sentiment.magnitude, "language": language}
+
+    return json.dumps(response_json, sort_keys=False)
