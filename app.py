973021a15585f60683cfbf03ea45123db145c39d
Mike Shimkus <mshimkus@gmail.com>
2019-11-18 12:06:48 -0800

    many updates

--- app.py
+++ app.py
@@ -21,50 +21,66 @@ def insult():
     url = 'https://evilinsult.com/generate_insult.php?lang=en&type=json'
     r = requests.get(url)
     j = r.json()
+
+    insult_str = j['insult']
+    remove_table = dict.fromkeys(map(ord, '+"@#$'), None)
+    insult_str = insult_str.translate(remove_table)
+    insult_str_len = len(insult_str)
+
+    print('Insult: ' + insult_str)
+    units = str(math.ceil((insult_str_len/1000)*2))
+    print('Number of Google Natural Language units used processing this insult: ' + units)
+
+    doc_data = analyze_sentiment(insult_str)
+    entity_data = []
+    entity_response = analyze_entity_sentiment(insult_str)
+    for entity in entity_response.entities:
+        sentiment = entity.sentiment
+        entity_data.append({'entity_name': entity.name, 'entity_score': sentiment.score, 'entity_magnitude': sentiment.magnitude, 'entity_salience': entity.salience})
+
+    html = render_template('insult.html', text=insult_str, score=doc_data.document_sentiment.score, magnitude=doc_data.document_sentiment.magnitude, entities=entity_data, usage=units)
     
-    sentiment_str = analyze_insult(j["insult"])
-    sentiment_json = json.loads(sentiment_str)
-
-    burn = sentiment_json["text"]
-    score = sentiment_json["sentiment_score"]
-    magnitude = sentiment_json["sentiment_magnitude"]
-
-    return render_template('insult.html', text=burn, score=score, magnitude=magnitude)
+    return html
 
 @app.route("/news")
 def news():
     subject = request.args.get('subject', default = '', type = str)
-    newsapikey = os.environ['NEWSAPIKEY']
+    news_api_key = os.environ['NEWSAPIKEY']
     url = ('https://newsapi.org/v2/top-headlines?'
        'q=' + subject + '&'
        'country=us&'
        'language=en&'
-       'apiKey=' + newsapikey)
+       'apiKey=' + news_api_key)
     r = requests.get(url)
     j = r.json()
-    new_str = ''
+    news_str = ''
 
     for k,v in j.items():
         if k == "articles":
             articles_json = v
             for article in articles_json:
                 description = str(article['description'])
-                new_str = new_str + description + ' '
+                news_str = news_str + description + ' '
     
     remove_table = dict.fromkeys(map(ord, '+"@#$'), None)
-    new_str = new_str.translate(remove_table)
-    new_str_len = len(new_str)
+    news_str = news_str.translate(remove_table)
+    news_str_len = len(news_str)
 
-    print('News: ' + new_str)
-    print('Number of Google Natural Language units used: ' + str(math.ceil((new_str_len/1000)*2)))
+    print('News: ' + news_str)
+    units = str(math.ceil((news_str_len/1000)*2))
+    print('Number of Google Natural Language units used processing this news: ' + units)
 
-    document_sentiment_data = analyze_sentiment(new_str)
-    entity_sentiment_data = analyze_entity_sentiment(new_str)
+    doc_data = analyze_sentiment(news_str)
+    entity_data = []
+    entity_response = analyze_entity_sentiment(news_str)
+    for entity in entity_response.entities:
+        sentiment = entity.sentiment
+        entity_data.append({'entity_name': entity.name, 'entity_score': sentiment.score, 'entity_magnitude': sentiment.magnitude, 'entity_salience': entity.salience})
 
     if subject == '':
         subject = 'everything'
 
-    html = render_template('news.html', subject=subject, score=document_sentiment_data[0]['sentiment'], magnitude=document_sentiment_data[0]['magnitude'], entities=entity_sentiment_data)
+    html = render_template('news.html', subject=subject, score=doc_data.document_sentiment.score, magnitude=doc_data.document_sentiment.magnitude, entities=entity_data, usage=units)
     
     return html
 
@@ -101,9 +117,7 @@ def analyze_sentiment(text_content):
     encoding_type = enums.EncodingType.UTF8
     response = client.analyze_sentiment(document, encoding_type=encoding_type)
 
-    data = [{'sentiment': response.document_sentiment.score,'magnitude': response.document_sentiment.magnitude, 'language': response.language}]
-
-    return data
+    return response
 
 def analyze_entity_sentiment(text_content):
     """
@@ -119,10 +133,5 @@ def analyze_entity_sentiment(text_content):
     document = {"content": text_content, "type": type_, "language": language}
     encoding_type = enums.EncodingType.UTF8
     response = client.analyze_entity_sentiment(document, encoding_type=encoding_type)
-    data = []
-
-    for entity in response.entities:
-        sentiment = entity.sentiment
-        data.append({'entity_name': entity.name, 'entity_score': sentiment.score, 'entity_magnitude': sentiment.magnitude, 'entity_salience': entity.salience})
  
-    return data
+    return response
